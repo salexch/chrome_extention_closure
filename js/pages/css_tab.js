e5bc6064649ff976f7bec01fd280d9a775a7bef2
Alex Chertin <alex@opentech.co.il>
2012-12-22 00:00:53 +0200

    image convertion improvment

--- js/pages/css_tab.js
+++ js/pages/css_tab.js
@@ -21,6 +21,7 @@ var StylesCollection = ScriptsCollection.extend({
 				
 				var script_obj = {
 					id: id,
+					src_full: '',
 					src: text.replace(/\s/ig, '').substring(0, 40),
 					text: text
 				};	
@@ -41,41 +42,52 @@ var StylesCollection = ScriptsCollection.extend({
 			dfd.resolve();
 		
 
-		$.each(selected, function() {
-			styles_text += YAHOO.compressor.cssmin(this.get('text'));
-		});
-
-		if (SETTINGS.image_convert)	 {
+		
+		if (SETTINGS.image_convert) {
 			var imgs_srcs = [];
 			var imgs_ids = [];
-			
-			console.groupCollapsed('Converting Images:'); 
-			
-			styles_text = styles_text.replace(/(\()([^)]{0,}(png|jpg|jpeg|PNG|JPG|JPEG))(\))/ig, function() {
-				var id = _.uniqueId('DataUriPlaceholder_');
-				console.log('#' + id + ' ', 'url(' + arguments[2] + ')');
-				
-				var url = arguments[2].replace(/\.{2}\//gi, ''); 
-				imgs_srcs.push(url);
-				imgs_ids.push(id);
-				
-				return '(' + id + ')';
-			});
+			console.groupCollapsed('Converting Images to base64:'); 
+		}
+		
+		$.each(selected, function() {
+			var that = this;
+			var compressed = YAHOO.compressor.cssmin(this.get('text'));
 
-			console.groupEnd();
+			if (SETTINGS.image_convert)	 {
+				console.groupCollapsed(that.get('src')); 
+				
+				styles_text += compressed.replace(/(\()([^)]{0,}(png|jpg|jpeg|PNG|JPG|JPEG))(\))/ig, function() {
+					var id = _.uniqueId('DataUriPlaceholder_');
+					var css_url = that.get('src_full').replace(/([^\/]+$)/ig, ''); //removes filename
+					var img_url = css_url + arguments[2];
+					
+					console.log(img_url);
+					
+					imgs_srcs.push(img_url);
+					imgs_ids.push(id);
+					
+					return '(' + id + ')';
+				});
+				
+				console.groupEnd();
+			} else {
+				styles_text += compressed;
+			}
 			
-			//$.when.apply( $, $.map( imgs_srcs, $.image ) ).then(function() { 
+		});	
+		
+		if (SETTINGS.image_convert) {
+			console.groupEnd();
 			$.when.apply( this, $.map( imgs_srcs, $.image ) ).then(function() {
 				$.each(arguments, function(i, img) {
 					styles_text = styles_text.replace(new RegExp(imgs_ids[i]), convertImageToBase64(img));
 				});
 				dfd.resolve(styles_text);
-			});
-			
+			});			
 		} else
 			_.defer(function() {
 				dfd.resolve(styles_text);
-			});
+			});	
 		
 		return dfd.promise();
 	}
